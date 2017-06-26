e761ff3d55688d775751ec27206f4169abd7dc59
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2017-06-26 12:59:13 +0300

    middlewares

--- webtask.js
+++ webtask.js
@@ -1,9 +1,15 @@
 const tools = require('auth0-extension-express-tools');
 
-const expressApp = require('./server/index');
+const expressApp = require('./server');
+const config = require('./server/lib/config');
 const logger = require('./server/lib/logger');
 
-module.exports = tools.createServer((config, storage) => {
+const createServer = tools.createServer((config, storage) => {
   logger.info('Starting Logs to Splunk extension - Version:', process.env.CLIENT_VERSION);
   return expressApp(config, storage);
 });
+
+module.exports = (context, req, res) => {
+  config.setValue('PUBLIC_WT_URL', tools.urlHelpers.getWebtaskUrl(req));
+  createServer(context, req, res);
+};
